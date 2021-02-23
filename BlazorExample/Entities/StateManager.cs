11afa024a2a1988d44c77e7b17defb131d7435e2
micke <miga02@gmail.com>
2021-02-23 18:49:58 +0100

    Testing state manager

---
+++ BlazorExample/Entities/StateManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorExample.Entities
+{
+  public class StateManager
+  {
+    public string Property { get; set; }
+    public int Counter { get; set; }
+
+    public event Action OnChange;
+
+    public void SetProperty(string property)
+    {
+      Property = property;
+      Counter++;
+      NotifyStateChanged();
+    }
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+  }
+}
--- BlazorExample/Program.cs
+++ BlazorExample/Program.cs
@@ -7,35 +7,37 @@ using Microsoft.Extensions.DependencyInjection;
 using BlazorExample.Components.DependencyInjection.Interfaces;
 using BlazorExample.Components.DependencyInjection.Implementations;
 using System.Net.Http;
+using BlazorExample.Entities;
 
 namespace BlazorExample
 {
-    public class Program
+  public class Program
+  {
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+      var builder = WebAssemblyHostBuilder.CreateDefault(args);
+      builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped<IConstructorInjectionOne, ConstructorInjectionOne>();
-            builder.Services.AddScoped<IConstructorInjectionTwo, ConstructorInjectionTwo>();
+      builder.Services.AddScoped<IConstructorInjectionOne, ConstructorInjectionOne>();
+      builder.Services.AddScoped<IConstructorInjectionTwo, ConstructorInjectionTwo>();
 
 
-            builder.Services.AddScoped<IScoped, Scoped>();
-            builder.Services.AddSingleton<ISingleton, Singleton>();
-            builder.Services.AddTransient<ITransient, Transient>();
+      builder.Services.AddScoped<IScoped, Scoped>();
+      builder.Services.AddSingleton<ISingleton, Singleton>();
+      builder.Services.AddTransient<ITransient, Transient>();
+      builder.Services.AddSingleton<StateManager>();
 
-            //****other variants for service registration on singleton example****
-            //builder.Services.AddSingleton<Scoped>();
+      //****other variants for service registration on singleton example****
+      //builder.Services.AddSingleton<Scoped>();
 
-            //var singleton = new Scoped();
-            //builder.Services.AddSingleton<IScoped>(singleton);
+      //var singleton = new Scoped();
+      //builder.Services.AddSingleton<IScoped>(singleton);
 
-            //****
+      //****
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+      builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
-        }
+      await builder.Build().RunAsync();
     }
+  }
 }
